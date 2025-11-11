25ffd56670e7a731f99d2510eece8ac87f7910de
Mackenzie Willard <Mackenzie.Willard@truetandem.com>
2025-11-10 20:18:09 -0500

    Setting up the question

---
+++ python/2025/11/10.py
@@ -0,0 +1,16 @@
+'''
+Extension Extractor
+
+Given a string representing a filename, return the extension of the file.
+ * The extension is the part of the filename that comes after the last period (.).
+ * If the filename does not contain a period or ends with a period, return "none".
+ * The extension should be returned as-is, preserving case.
+
+Test Case
+1. get_extension("document.txt") should return "txt".
+2. get_extension("README") should return "none".
+3. get_extension("image.PNG") should return "PNG".
+4. get_extension(".gitignore") should return "gitignore".
+5. get_extension("archive.tar.gz") should return "gz".
+6. get_extension("final.draft.") should return "none".
+'''
